9ed43f69ce6d0fe016f69af0ad99cd612bd6c158
Farhaan Patel <patel.farhaaan@gmail.com>
2020-03-18 21:45:14 -0700

    authenticated logic done

--- common_utilities/primary_mongo_write.py
+++ common_utilities/primary_mongo_write.py
@@ -19,18 +19,3 @@ def insert_multiple_obj_in_database(database, collection, data_obj):
     db = mongo_client[database]
     collection = db[collection]
     return collection.insert_many(data_obj).acknowledged
-
-
-database = "images"
-collection = "companies"
-data_obj1 = {
-    "company_name" : "farhaans.com",
-    "logo_url" : "https://angelfund-company-images.s3-us-west-1.amazonaws.com/tesla.jpg"
-}
-data_obj2 = {
-    "company_name" : "farhaan.com",
-    "logo_url" : "https://angelfund-company-images.s3-us-west-1.amazonaws.com/tesla.jpg"
-}
-
-resp = insert_multiple_obj_in_database(database, collection, [data_obj1, data_obj2])
-print(resp)
--- common_utilities/secondary_mongo_read.py
+++ common_utilities/secondary_mongo_read.py
@@ -12,18 +12,10 @@ def search_single_obj_in_database(database, collection, query_obj):
     collection = db[collection]
     return collection.find_one(query_obj)
 
+
 def search_multiple_obj_in_database(database, collection, query_obj):
     remote_mongo_uri = CONSTANT.SECONDARY_DB_CLUSTER.value
     mongo_client = MongoClient(remote_mongo_uri)
     db = mongo_client[database]
     collection = db[collection]
     return list(collection.find(query_obj))
-
-
-database = "images"
-key = "company_name"
-value = "google.com"
-collection = "companies"
-query_obj = {key: value}
-resp = search_single_obj_in_database(database, collection, query_obj)
-print(resp)
