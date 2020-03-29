43af40db403373c30288d25f672738ee69987353
Yuzhu Li <liyuzhumail@gmail.com>
2020-03-29 01:24:21 -0700

    add email_confirmed to user obj schema

--- project/investor/marshmallow_serialize.py
+++ project/investor/marshmallow_serialize.py
@@ -4,4 +4,5 @@ from project import ma
 class InvestorUserSchema(ma.Schema):
     class Meta:
         fields = ("first_name", "last_name", "email", "sectors", "deals",
-                  "bio", "location", "accreditation", "syndicate", "angel")
+                  "bio", "location", "accreditation", "syndicate", "angel", 
+                  "email_confirmed")
