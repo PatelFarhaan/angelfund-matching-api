590c2d652e2e88668cc952c6c386f2f8e47d8350
Yuzhu Li <liyuzhumail@gmail.com>
2020-03-31 23:11:16 -0700

    add account_approved

--- project/investor/marshmallow_serialize.py
+++ project/investor/marshmallow_serialize.py
@@ -5,4 +5,4 @@ class InvestorUserSchema(ma.Schema):
     class Meta:
         fields = ("first_name", "last_name", "email", "sectors", "deals",
                   "bio", "location", "accreditation", "syndicate", "angel", 
-                  "email_confirmed")
+                  "email_confirmed", "account_approved")
