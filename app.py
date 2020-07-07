338fcac0d8e21a5ada04c54b7715434b1e025a2a
Farhaan <patel.farhaaan@gmail.com>
2020-07-06 18:06:49 -0700

    ssl added

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
     logger.info(f"flask reserver started at {datetime.datetime.utcnow()}")
-    app.run(debug=True, use_reloader=True, host='127.0.0.1', port=5000)
+    app.run(ssl_context='adhoc', host='127.0.0.1', port=5000)
 
