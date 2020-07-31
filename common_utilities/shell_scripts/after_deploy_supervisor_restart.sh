8b4e4d0937feb5eea51953d5f7964a584ba38a59
Farhaan <patel.farhaaan@gmail.com>
2020-07-31 14:01:19 -0700

    shell scripts path updated

--- common_utilities/shell_scripts/after_deploy_supervisor_restart.sh
+++ common_utilities/shell_scripts/after_deploy_supervisor_restart.sh
@@ -3,7 +3,7 @@
 cd /var/flaskapp/flask
 sudo bash compiled_files_cleanup.sh
 sudo chmod +x start.sh
-cd  /var/flaskapp/flask/common_utilities/scripts
+cd  /var/flaskapp/flask/common_utilities/shell_scripts
 sudo chmod +x secondary_upgrade.sh
 sudo chmod +x secondary_replicate.sh
 sudo systemctl restart supervisor.service
